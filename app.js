863946dad1550181aed16ba060f3d6bbe2427fe2
Maria Silla <maria.sylla@gmail.com>
2018-07-25 08:30:55 -0400

    added android crud

--- app.js
+++ app.js
@@ -96,17 +96,6 @@ app.get('/operator_profile/', function(req, res){
       "logged_in": true,
       "email": req.session.operator.email
     };
-   //  console.log(data.zipcode);
-    //https://api.meetup.com/2/open_events?key=125567365e1835372f462b14a4a2d41&sign=true&photo-host=public&zip=10003&text=painter&page=20
-    // var url = 'https://api.meetup.com/2/open_events?key=125567365e1835372f462b14a4a2d41&sign=true&photo-host=public&zip=';
-    // var search = '&text=painting&page=20';
-   //  var url = 'https://api.meetup.com/find/events?key=125567365e1835372f462b14a4a2d41&sign=true&photo-host=public&text=painter&page=20';
-   //display API's data according to user's zipcode
-    // axios.get(url+data.zipcode+search)
-    // .then(function(response){
-     //  data.meetup = response.data
-      // data.meetup = response.data.results
-    //  console.log(data.meetup);
     res.render('operator_profile/operator', data);
   // })
   } else {
@@ -114,9 +103,128 @@ app.get('/operator_profile/', function(req, res){
   }
 })
 
+//Render all Androids to one page
+app.get('/androids/', function(req, res){
+  var data;
+
+  if(req.session.operator){
+       data = {
+      "logged_in": true,
+      "email": req.session.operator.email
+      };
+
+    db
+       .any("SELECT * FROM androids")
+       .then(function(info){
+          androids_array = {
+           androids: info,
+           security: data
+          }
+
+        res.render('androids/allandroids', androids_array)
+     })
+    // res.render('profile/portfolio', data);
+  } else {
+    res.render('home/index');
+  }
+})
 
 
+//Render add_new_android form
+app.get('/add_new_android', function(req, res){
+  var data ;
+  if(req.session.operator){
+    data = {
+      "logged_in": true,
+      "email": req.session.operator.email
+    };
+    res.render('androids/add_new_android', data);
+  } else {
+    res.render('home/index');
+  }
+});
 
+// Register a new Android
+app.post('/add_new_android', function(req,res){
+  var data;
+  if(req.session.operator){
+    console.log(req.body);
+    data = req.body;
+
+                db.none(
+                  "INSERT INTO androids (name, avatar, skills, reliability, status, job_id) VALUES ($1, DEFAULT, $2, DEFAULT, $3, DEFAULT)",
+                  [data.name, data.skills, data.status]
+                ).catch(function(e){
+                  res.send('Failed to register: ' + e + 'Please go back and try again');
+
+                }).then(function(){
+                  // res.render('profile/portfolio', data);
+                  res.redirect("/androids");
+                });
+
+       } else {
+          res.render('home/index');
+       }
+  });
+
+  // Delete an Android
+  app.delete('/delete_android/:id', function(req, res){
+    console.log(req.params.id)
+        var id = req.params.id;
+        if(req.session.operator){
+          db.none(
+              "DELETE FROM androids WHERE id = $1", id
+          ).then(function(){
+          res.redirect("/androids");
+          });
+
+        } else {
+          res.render('home/index');
+        }
+  })
+
+
+  //Render update_android form
+  app.get('/update_android/:id', function(req, res){
+    var data ;
+    var id = req.params.id;
+    console.log(id);
+    if(req.session.operator){
+      data = {
+        "logged_in": true,
+        "email": req.session.operator.email,
+        "id": id
+      };
+      res.render('androids/update_android', data);
+    } else {
+      res.render('home/index');
+    }
+  });
+
+
+  //Update Android info
+  app.put('/update_android/:id', function(req, res){
+    var id = req.params.id;
+    console.log(id);
+    var data = req.body;
+    db
+      .none("UPDATE androids SET name = $1 WHERE id = $2",
+        [data.name, id]
+      ).catch(function(){
+        res.send('Failed to update android. Please go back and try again.');
+      }).then(function(){
+        res.redirect("/androids");
+        // res.send('User updated.');
+      });
+
+  });
+
+
+//Logout
+app.get('/logout', function(req, res){
+  req.session.operator = false;
+  res.redirect("/");
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Server running, listening on port 3000!');
